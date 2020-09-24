86efb41c8b0f9d79474f81690e7625ec424fcad7
Dirk <dirkthomasmulder@gmail.com>
2020-09-24 09:39:44 +0700

    changed $ in jQuery

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-$(document).ready(function () {
+jQuery(document).ready(function () {
     // Make sure that the "next" and "prev" buttons are translated into Dutch
     jQuery('.page-next').text('Volgende');
     jQuery('.page-prev').text('Vorige');
 
     // Homepage read more button
-    $(".more-text").hide();
-    $(".show_hidden").on("click", function () {
-        var txt = $(".content").is(':visible') ? 'Read More' : 'Read Less';
-        $(".show_hidden").text(txt);
-        $(this).next('.more-text').slideToggle(200);
+    jQuery(".more-text").hide();
+    jQuery(".show_hidden").on("click", function () {
+        var txt = jQuery(".content").is(':visible') ? 'Read More' : 'Read Less';
+        jQuery(".show_hidden").text(txt);
+        jQuery(this).next('.more-text').slideToggle(200);
     });
 
 });
