1c148c0410a5eb4ad89757530e214f65aab36382
Dirk <dirkthomasmulder@gmail.com>
2020-09-24 09:37:37 +0700

    added several read more JS code snippets

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-// Make sure that the "next" and "prev" buttons are translated into Dutch
-jQuery('.page-next').text('Volgende');
-jQuery('.page-prev').text('Vorige');
+$(document).ready(function () {
+    // Make sure that the "next" and "prev" buttons are translated into Dutch
+    jQuery('.page-next').text('Volgende');
+    jQuery('.page-prev').text('Vorige');
+
+    // Homepage read more button
+    $(".more-text").hide();
+    $(".show_hidden").on("click", function () {
+        var txt = $(".content").is(':visible') ? 'Read More' : 'Read Less';
+        $(".show_hidden").text(txt);
+        $(this).next('.more-text').slideToggle(200);
+    });
+
+});
