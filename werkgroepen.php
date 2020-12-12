ae9b97aa7e24dfaf1554a3edb3f0f82610869bb7
Dirk <dirkthomasmulder@gmail.com>
2020-12-12 07:08:13 +0700

    new changes

--- werkgroepen.php
+++ werkgroepen.php
@@ -1,69 +1,81 @@
-<?php 
+    <?php 
 
-function werkgroep_function() {
-    $args = array( 'post_type' => 'werkgroepen', 'posts_per_page' => 100);
-    $loop = new WP_Query( $args );
+    function werkgroep_function() {
+        $args = array( 'post_type' => 'werkgroepen', 'posts_per_page' => 100, 'orderby'=>'title', 'order'=>'ASC');
+        $loop = new WP_Query( $args );
 
-       $html .= '<div class="werkgroepen">';
-    
-    foreach ($loop->posts as $post)
-    {
-        $html .= '
-        <div id="' . (str_replace(' ', '-', strtolower($post->post_title))) . '" class="werkgroep">
-            <h2>' . $post->post_title . '</h2> 
-
-            <p>' . get_field("beschrijving",$post->ID) . '</p>';
-
-            
-            if( have_rows('contactpersonen',$post->ID) ):
-
-                 // Loop through rows.
-                    while( have_rows('contactpersonen',$post->ID) ) : the_row();
-
-                    // Load sub field value.
-                    $sub_value = get_sub_field('naam');
-
-                    
-                    $html .= '
-                    
-                    <h5 class="functie"> ' . get_sub_field('functie') . ' </h5>
-                    <p><strong>' . get_sub_field('naam') .'</strong></p>
-                    
-                    <ul class="werkgroep-contact">';
-                        
-                        if( get_sub_field('telefoonnummer') ) {
-                            $html .= '<li><ion-icon name="call-outline"></ion-icon>' . get_sub_field('telefoonnummer') .'</li>';
-                        };
-
-                        if( get_sub_field('mobiel_nummer') ) {
-                            $html .= '<li><ion-icon name="phone-portrait-outline">' . get_sub_field('mobiel_nummer') .'</li>';
-                        };
-
-                        if( get_sub_field('email') ) {
-                            $html .= '<li><ion-icon name="mail-outline"></ion-icon></ion-icon>' . get_sub_field('email') .'</li>';
-                        };
-
-                    $html .= '
-                    </ul>
-                    ';
-                    // Do something...
-
-                // End loop.
-                endwhile;
-
-            endif;
-
-            $html .= '
-        </div>
+        $html .= '<div class="werkgroepen">';
         
-        '; 
-}   
+        foreach ($loop->posts as $post)
+        {
+            $html .= '
+            <div id="' . (str_replace(' ', '-', strtolower($post->post_title))) . '" class="werkgroep">
+                <h2>' . $post->post_title . '</h2> ';
 
-    $html .= '</div>';
+                if( get_field("beschrijving",$post->ID) ) {
+                    $html .= '<p>' . get_field("beschrijving",$post->ID) . '</p>';
+                };
 
-return $html;
-wp_reset_query();
+                
+                if( have_rows('contactpersonen',$post->ID) ):
+                    $html .= '<div class="personen__container">';
+                    // Loop through rows.
+                        while( have_rows('contactpersonen',$post->ID) ) : the_row();
 
-} 
+                        $html .= '<div class="contactpersonen__persoon">';
 
-add_shortcode('werkgroepen', 'werkgroep_function');
+                        // Load sub field value.
+                        $sub_value = get_sub_field('naam');
+
+                        
+                        $html .= '
+                        
+                        <h5 class="functie"> ' . get_sub_field('functie') . ' </h5>
+                        <p class="contactpersoon__name">' . get_sub_field('naam') .'</p>
+                        
+                        <ul class="werkgroep-contact">';
+                            
+                            if( get_sub_field('telefoonnummer') ) {
+                                $html .= '<li><ion-icon name="call-outline"></ion-icon>' . get_sub_field('telefoonnummer') .'</li>';
+                            };
+
+                            if( get_sub_field('mobiel_nummer') ) {
+                                $html .= '<li><ion-icon name="phone-portrait-outline">' . get_sub_field('mobiel_nummer') .'</li>';
+                            };
+
+                            if( get_sub_field('email') ) {
+                                $html .= '<li><ion-icon name="mail-outline"></ion-icon></ion-icon>
+                                <a href="mailto:' . get_sub_field('email') .'">'
+
+                                . get_sub_field('email') .
+                                
+                                '</a></li>';
+                            };
+
+                        $html .= '
+                        </ul>
+                        ';
+                        // Do something...
+
+                        $html .= '</div>';
+                        //end div
+                    
+                    // End loop.
+                    endwhile;
+                    $html .= '</div>';
+                endif;
+
+                $html .= '
+            </div>
+            
+            '; 
+    }   
+
+        $html .= '</div>';
+
+    return $html;
+    wp_reset_query();
+
+    } 
+
+    add_shortcode('werkgroepen', 'werkgroep_function');
